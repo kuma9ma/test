23d0fcd5ea99e28dd21a2c34dcc83bf3b58c01b7
山田優太 <>
2023-09-20 17:01:00 +0900

    コミット

---
+++ Views/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="../Views/css/style.css">
+</head>
+<body>
+    <header class="header">
+        <h1 class="header_logo"><a href="#">Blog</a></h1>
+        <nav class="header_nav">
+            <ul>
+                <li><a href="#">about</a></li>
+                <li><a href="#">blog</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+        
+    </main>
+</body>
+</html>
